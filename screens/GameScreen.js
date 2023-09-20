3b9c902f9f1c71dda5bd8e0e1e8fb22c666a8557
aadil42 <adilkelawala@gmail.com>
2023-09-20 14:09:00 +0530

    adding screens.

---
+++ screens/GameScreen.js
@@ -0,0 +1,11 @@
+import {View, TextInput} from 'react-native';
+
+const GameScreen = () =>  {
+    return (    
+        <View>
+            <TextInput />
+        </View>
+    );
+}
+
+export default GameScreen;
