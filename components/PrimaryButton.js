7d292e113370ba9f803fa7c4af34d0216c4dfd6b
aadil42 <adilkelawala@gmail.com>
2023-09-20 14:11:50 +0530

    adding components

---
+++ components/PrimaryButton.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+
+const PrimaryButton = ({title}) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    );
+}
+
+export default PrimaryButton;
